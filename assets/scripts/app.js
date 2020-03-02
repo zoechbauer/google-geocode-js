d1601990a5ed1f99c1c112b66bf357769c82226a
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-02 12:57:06 +0100

    display formatted address in browser

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,21 @@ function geocode() {
         key: api_key
       }
     })
-    .then(result => console.log(result))
+    .then(result => {
+      console.log(result);
+
+      // formatted address
+      const formattedAddress = result.data.results[0].formatted_address;
+      const formattedAddressOutput = `
+        <ul class="list-group">
+          <li class="list-group-item">${formattedAddress}</li>
+        </ul>
+      `;
+
+      // output to app
+      document.getElementById(
+        'formatted-address'
+      ).innerHTML = formattedAddressOutput;
+    })
     .catch(err => console.log(err));
 }
