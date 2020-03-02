e5f85f935e143f1ca69b3005a78fabc099d1cb3e
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-02 13:44:43 +0100

    display address components and geometry

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,21 +7,47 @@ function geocode() {
         key: api_key
       }
     })
-    .then(result => {
-      console.log(result);
+    .then(response => {
+      console.log(response);
 
       // formatted address
-      const formattedAddress = result.data.results[0].formatted_address;
+      const formattedAddress = response.data.results[0].formatted_address;
       const formattedAddressOutput = `
         <ul class="list-group">
           <li class="list-group-item">${formattedAddress}</li>
         </ul>
       `;
 
+      // address components
+      const addressComponents = response.data.results[0].address_components;
+      let addressComponentsOutput = '<ul class="list-group">';
+      for (let i = 0; i < addressComponents.length; i++) {
+        addressComponentsOutput += `
+          <li class="list-group-item"><strong>
+            ${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}
+          </li>
+        `;
+      }
+      addressComponentsOutput += '</ul>';
+
+      // geometry
+      const lat = response.data.results[0].geometry.location.lat;
+      const lng = response.data.results[0].geometry.location.lng;
+      const geometryOutput = `
+        <li class="list-group-item">
+          <strong>latitude</strong>: ${lat}</li>  
+        <li class="list-group-item">
+          <strong>longitude</strong>: ${lng}</li>
+      `;
+
       // output to app
       document.getElementById(
         'formatted-address'
       ).innerHTML = formattedAddressOutput;
+      document.getElementById(
+        'address-components'
+      ).innerHTML = addressComponentsOutput;
+      document.getElementById('geometry').innerHTML = geometryOutput;
     })
     .catch(err => console.log(err));
 }
